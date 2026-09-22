1eb62c13fe90333cccbd5313247938d7d43da2c7
pedroangel62-blip <pedro_angel62@hotmail.com>
2026-09-22 23:43:09 +0200

    Close the Windows test client cleanly

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,6 +20,7 @@ class AplicacionTests(unittest.TestCase):
         self.cliente = TestClient(main.app)
 
     def tearDown(self):
+        self.cliente.close()
         main.DIRECTORIO_PROYECTOS = self.directorio_anterior
         self.directorio_temporal.cleanup()
 
